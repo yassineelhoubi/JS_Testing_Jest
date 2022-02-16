247a7b56a109606801b7c91487fb6156c9970727
yassineelhoubi <elhoubiyassine@gmail.com>
2022-02-16 22:40:17 +0100

    Setting Up && Tearing Down Tests

---
+++ 03-setup/setup.test.js
@@ -0,0 +1,23 @@
+let animals = ['elephant', 'zebra', 'bear', 'tiger']
+
+
+describe('animals', () => {
+    it("should add animal to end of array", () => {
+        animals.push("alligator");
+        expect(animals[animals.length - 1]).toBe("alligator");
+    })
+
+    it("should add animal to beginning of array", () => {
+        animals.unshift("monkey");
+        expect(animals[0]).toBe("monkey");
+    })
+
+    it("should have initial length of 4", () => {
+        expect(animals.length).toBe(4);
+    })
+})
+describe("testing something else", () => {
+    it.only("true should be truthy", () => {
+        expect(true).toBeTruthy();
+    })
+})
